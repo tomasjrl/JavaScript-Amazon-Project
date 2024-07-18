0d23906121f2e77ad06f1d319033fa9bcbeba990
tomasjrl <tstabilini@gmail.com>
2024-07-18 00:15:11 -0300

    integro localStorage para guardar cambios en lista de productos

--- data/cart.js
+++ data/cart.js
@@ -2,8 +2,14 @@
 // export permite llevar la variable declarada fuera del archivo js
 // y se aplica declarando type="module" en la etiqueta script del html
 // los modules declarados para probarlos funcionan solo con live-server (y no abriendo el archivo desde el explorador)
+// llamo a localStorage con getItem dandole el parametro de la variable cart para que tome el contenido guardado
+// uso JSON.parse para reconvertir en un array el string guardado previamente con JSON.stringify 
+// si cart esta vacio de contenido (!cart) le dara un valor determinado 
 
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+ cart = [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
@@ -13,6 +19,16 @@ export let cart = [
     quantity: 1,
   },
 ];
+}
+
+// creamos funcion para guardar info localstorage
+// al refrescar la pagina se debe mantener los cambios realizados
+// el 1er parametro 'cart' es por la variable cart
+// el 2do parametro, convierte en string el contenido de la variable cart con JSON.stringify
+
+function saveToStorage(){
+localStorage.setItem('cart', JSON.stringify(cart));
+};
 
 // compilando funciones para mejor lectura y aplicandolos (llamandolos) luego en el codigo
 // se usa parametro productId para pasar el valor button.dataset.productId y llevarlo a la funcion
@@ -24,6 +40,7 @@ export let cart = [
 // creamos una variable matchingItem indefinida fuera del scope para poder usarla posteriormente
 // si ya existe el producto, se incrementa quantity +1
 // si no existe (else) carga el producto a cart
+// llamo al final a la funcion saveToStorage
 
 export function addToCart(productId) {
   let matchingItem;
@@ -42,6 +59,8 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+
+  saveToStorage();
 }
 
 // creo funcion para remover un producto de checkout al apretar boton remove
@@ -49,6 +68,7 @@ export function addToCart(productId) {
 // el loop contendra toda la lista de productos que NO coincidan con el productId (que son los eliminados)
 // y creara un nuevo array con solo los productos restantes
 // export la funcion para aplicarla en checkout.js
+// llamo a la funcion saveToStorage
 
 export function removeFromCart(productId) {
   const newCart = [];
@@ -60,4 +80,6 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
 }
