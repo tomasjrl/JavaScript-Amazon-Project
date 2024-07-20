0335757760a656e5dcabaafef062cda4f51d077a
tomasjrl <tstabilini@gmail.com>
2024-07-20 02:21:42 -0300

    CORREGIDO SHIPPING AHORA FUNCIONA (.deliveryOptionId era correcto) (estaba getDeliveryOptionId)

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -18,7 +18,7 @@ export function renderPaymentSummary() {
     const product = getProduct(cartItem.productId);
     productPriceCents += product.priceCents * cartItem.quantity;
 
-    const deliveryOption = getDeliveryOption(cartItem.getDeliveryOptionId);
+    const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
     shippingPriceCents += deliveryOption.priceCents;
   });
 
