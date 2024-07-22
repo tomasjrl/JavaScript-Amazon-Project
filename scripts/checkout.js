23069a1d3346256ab63c4ef53d4c40eba02c13ee
tomasjrl <tstabilini@gmail.com>
2024-07-22 03:26:37 -0300

    creando oop version de cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,7 @@
 
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import '../data/cart-oop.js';
 
 //corre la funcion que actualiza la data y regenera el html instantaneamente con fechas asignadas
 // tecnica MVC = model view controller que interactuan en orden con un loop
